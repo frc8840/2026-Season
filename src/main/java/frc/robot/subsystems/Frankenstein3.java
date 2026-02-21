2a27016fd552ecfd8102cbbdac2120c9481baf4c
team8840 <projectcenter@bayschoolsf.org>
2026-02-21 12:03:14 -0800

    intake 2 motors basic setup

--- src/main/java/frc/robot/subsystems/Frankenstein3.java
+++ src/main/java/frc/robot/subsystems/Frankenstein3.java
@@ -6,36 +6,47 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Logger;
 
 public class Frankenstein3 extends SubsystemBase {
 
-  private TalonFX frankMotor;
+  private double openPosition = 5;
+  private double closedPosition = 0;
+  private double spinnerSpeed =7;
+  private TalonFX intakePosition;
+  private TalonFX spinner;
   private TalonFXConfiguration frankConfig = new TalonFXConfiguration();
  
   public void FrankSubsystem() {
 
-    frankMotor = new TalonFX(38);
-
+    intakePosition = new TalonFX(38);
+    spinner = new TalonFX(38);
     // set up the motor configs
     frankConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
     frankConfig.CurrentLimits.SupplyCurrentLimit = 70; // this is the default
     frankConfig.CurrentLimits.SupplyCurrentLimitEnable = Constants.Swerve.supplyCurrentLimitEnable;
 
     // now set up the motor
-    frankMotor.getConfigurator().apply(frankConfig);
+    intakePosition.getConfigurator().apply(frankConfig);
+    spinner.getConfigurator().apply(frankConfig);
 
     // now the motor is ready to be controlled
   }
 
-  public void setSpeed(double speed) {
-    frankMotor.set(speed);
+  public void spin(){
+    spinner.set(spinnerSpeed);
   }
 
-  public void say67() {
-    Logger.Log("67!");
+  public void stop(){
+    spinner.set(0);
+
   }
 
+  public void setPositionOpen() {
+    intakePosition.setPosition(openPosition);
+  }
+  public void setPositionClosed() {
+    intakePosition.setPosition(closedPosition);
+  }
 
   @Override
   public void periodic() {
