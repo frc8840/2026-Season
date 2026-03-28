fa645c20f85f50c2d0aadd90e8cba1fa8fc1c3a0
team8840 <projectcenter@bayschoolsf.org>
2026-03-28 12:09:37 -0700

    auto edits + more sim stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -54,9 +54,10 @@ public class RobotContainer {
     Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
         new Pose2d(0, 0, new Rotation2d(0)),
         List.of(
-            new Translation2d(1, 0),
-            new Translation2d(1, -1)),
-        new Pose2d(2, -1, new Rotation2d(0)),
+            new Translation2d(0.5, 0),
+            new Translation2d(0.5, -0.5),
+            new Translation2d(-0.5, 0.5)),
+        new Pose2d(0.5, 0, new Rotation2d(90)),
         trajectoryConfig);
     Logger.Log("trajectory generated!");
     Logger.Log("" + trajectory);
